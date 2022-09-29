a91531b5f8c1db8d2c495d402fac16b0edb2c0a9
Ethan Garrison <ethang994@gmail.com>
2022-09-29 11:13:41 -0400

    Update backdoor-faction script to report num of missing ports

--- src/scripts/singularity/backdoor-factions.ts
+++ src/scripts/singularity/backdoor-factions.ts
@@ -1,49 +1,63 @@
 import { buildServerTree, buildPath } from "scripts/utils/ns-utils"
-import { Server } from "../../../NetscriptDefinitions"
+import { Player, Server } from "../../../NetscriptDefinitions"
+
+const home = "home"
+
+const serverFactionMap = {
+    "CSEC": "CyberSec",
+    "avmnite-02h": "NiteSec",
+    "I.I.I.I": "The Black Hand",
+    "run4theh111z": "BitRunners"
+}
+
+function connectToServer(ns: typeof NS, src: string, dest: string) {
+    const serverTree = buildServerTree(ns, src)
+    const path = buildPath(serverTree, dest)
+    ns.tprint(`Attempting to connect to ${dest} from ${src} through path ${path.join(",")}`)
+    for (const node of path) {
+        if (!ns.singularity.connect(node)) {
+            ns.toast(`Failed to connect to ${node} while traversing ${path}`, "error")
+            return false
+        }
+    }
+    return true
+}
+
+async function autoInstallBackdoor(ns: typeof NS, server: Server, player: Player, faction?: string) {
+    const currentServerName = ns.singularity.getCurrentServer()
+    if (server.backdoorInstalled && server.hostname != home) {
+        ns.tprint(`Skipping ${server.hostname}, backdoor already installed`)
+        return false
+    }
+    if (!(server.hasAdminRights && server.requiredHackingSkill <= player.skills.hacking)) {
+        const portsRequired = server.numOpenPortsRequired - server.openPortCount
+        const hackSkill = server.requiredHackingSkill
+        ns.tprint(`Skipping ${server.hostname}, no root access (missing ${portsRequired} ports), or not enough hack levels (required ${hackSkill})`)
+        return false
+    }
+
+    if(!connectToServer(ns, currentServerName, server.hostname)) return false
+
+    if (server.hostname != home) {
+        ns.tprint("Installing backdoor...")
+        await ns.singularity.installBackdoor()
+        ns.tprint("Backdoor installed, attempting to join faction...")
+        while (!(player.factions.includes(faction) || ns.singularity.joinFaction(faction))) await ns.sleep(1000)
+        ns.tprint("Faction joined!")
+    }
+    else ns.tprint("Arrived back at home!")
+
+    return true
+}
 
 export async function main(ns: typeof NS) {
-    const home = "home"
-    const singularity = ns.singularity
     const player = ns.getPlayer()
 
-    const factionServers = [
-        {server: "CSEC", faction: "CyberSec"},
-        {server: "avmnite-02h", faction: "NiteSec"},
-        {server: "I.I.I.I", faction: "Black Hand"},
-        {server: "run4theh111z", faction: "BitRunners"},
-        {server: home, faction: ""}
-    ]
-    let currentServer: Server = ns.getServer(home) // Where we are currently
-    for(const {server,faction} of factionServers) {
-        const serverInfo = ns.getServer(server) // Where we are heading
-        if(serverInfo.backdoorInstalled && server != home) {
-            ns.tprint(`Skipping ${server}, backdoor already installed`)
-            continue
-        }
-        if(!(serverInfo.hasAdminRights && serverInfo.requiredHackingSkill <= player.skills.hacking)) {
-            ns.tprint(`Skipping ${server}, no root access, or not enough hack levels (required ${serverInfo.requiredHackingSkill})`)
-            continue
-        }
+    const factionServers = ["CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z"]
 
-        const serverTree = buildServerTree(ns, currentServer.hostname)
-        const path = buildPath(serverTree, server)
-        ns.tprint(`Attempting to connect to ${server} from ${currentServer.hostname} through path ${path.join(",")}`)
-        for(const node of path) {
-            if(!singularity.connect(node)) {
-                ns.toast(`Failed to connect to ${node} while traversing ${path}`, "error")
-                return
-            }
-        }
+    for (const server of factionServers) await autoInstallBackdoor(ns, ns.getServer(server), player, serverFactionMap[server])
 
-        // Update our currentServer info with the server we just arrived at
-        currentServer = serverInfo
-        if(server != home) {
-            ns.tprint("Installing backdoor...")
-            await singularity.installBackdoor()
-            ns.tprint("Backdoor installed, attempting to join faction...")
-            while(!(player.factions.includes(faction) || singularity.joinFaction(faction))) await ns.sleep(1000)
-            ns.tprint("Faction joined!")
-        }
-        else ns.tprint("Arrived back at home!")
-    }
+    // Head home
+    const currentServerName = ns.singularity.getCurrentServer()
+    if(currentServerName != home) connectToServer(ns, currentServerName, home)
 }
